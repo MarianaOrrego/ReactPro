7f7ba2f2796f537ccb23a485e946a158beda4727
Mariana Orrego <83648336+MarianaOrrego@users.noreply.github.com>
2024-03-15 14:33:34 -0500

    Refactor component patterns and styles

--- 02-react-app/src/02-component-patterns/components/ProductButtons.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductButtons.tsx
@@ -1,24 +1,24 @@
 import { useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
-import styles from '../styles/styles.module.css'
-
+import styles from "../styles/styles.module.css";
 
 export const ProductButtons = () => {
+  const { increaseBy, counter } = useContext(ProductContext);
 
-    const { increaseBy, counter } = useContext( ProductContext );
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+        {" "}
+        -{" "}
+      </button>
 
-    return (
-        <div className={ styles.buttonsContainer }>
-            <button
-                className={ styles.buttonMinus }
-                onClick={ () => increaseBy( -1 ) }> - </button>
+      <div className={styles.countLabel}> {counter} </div>
 
-            <div className={ styles.countLabel }> { counter } </div>
-
-            <button
-                className={ styles.buttonAdd }
-                onClick={ () => increaseBy( +1 ) }> + </button>
-        </div>
-    );
-}
+      <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+        {" "}
+        +{" "}
+      </button>
+    </div>
+  );
+};
--- 02-react-app/src/02-component-patterns/components/ProductImage.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductImage.tsx
@@ -1,25 +1,20 @@
-import { useContext } from 'react';
-import { ProductContext } from './ProductCard';
+import { useContext } from "react";
+import { ProductContext } from "./ProductCard";
 
-import styles from '../styles/styles.module.css'
-import noImage from '../assets/no-image.jpg';
+import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
 
+export const ProductImage = ({ img = "" }) => {
+  const { product } = useContext(ProductContext);
+  let imgToShow: string;
 
-export const ProductImage = ({ img = '' }) => {
+  if (img) {
+    imgToShow = img;
+  } else if (product.img) {
+    imgToShow = product.img;
+  } else {
+    imgToShow = noImage;
+  }
 
-    const { product } = useContext( ProductContext );
-    let imgToShow: string;
-
-    if ( img ) {
-        imgToShow = img;
-    } else if ( product.img ) {
-        imgToShow = product.img
-    } else {
-        imgToShow = noImage;
-    }
-
-
-    return (
-        <img className={ styles.productImg } src={ imgToShow } alt="Product" />
-    );
-}
+  return <img className={styles.productImg} src={imgToShow} alt="Product" />;
+};
